2f9db041413e330385d6d11f36e88392ee4466a7
alidev313xA <ali.dev423@gmail.com>
2025-06-03 01:34:01 +0500

    Update day1.js

--- day1.js
+++ day1.js
@@ -1,17 +1,24 @@
-
-
 /**
  * @param {number} n
  * @param {number} limit
  * @return {number}
  */
-var distributeCandies = function (n, limit) {
-    let ans = 0;
-    for (let i = 0; i <= Math.min(limit, n); i++) {
-        if (n - i > 2 * limit) {
-            continue;
-        }
-        ans += Math.min(n - i, limit) - Math.max(0, n - i - limit) + 1;
+// O(n) solution 
+var distributeCandies = function(n, limit) {
+    let ways = 0; 
+
+    // for child1 
+    let minCh1 = Math.max(0, n - 2 * limit); 
+    let maxCh2 = Math.min(n, limit); 
+
+    for (let i = minCh1; i <= maxCh2; i++) { // fixing the candies assignment for child 1 
+        let N = n - i; // remaining cadies to be assigned to chil 2 & 3 
+        
+        let minCh2 = Math.max(0, N - limit);
+        let maxCh2 = Math.min(N, limit); 
+
+        ways += maxCh2 - minCh2 + 1;  
     }
-    return ans;
+
+    return ways; 
 };
